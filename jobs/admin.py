c933384730878ff5447b9c7399e803b5a8a8c022
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-08 12:29:20 +0000

    added jobs app

---
+++ jobs/admin.py
@@ -0,0 +1,6 @@
+from jobs.models import Company, Job, Tag
+from django.contrib import admin
+
+admin.site.register(Job)
+admin.site.register(Company)
+admin.site.register(Tag)
---
+++ jobs/models.py
@@ -0,0 +1,39 @@
+from django_jobs.users.models import SENIORITY
+from django.db import models
+
+JOB_TYPE=(
+    ('remote','remote'),
+    ('remote-only','remote-only'),
+    ('not-remote','not-remote'),
+)
+
+class Job(models.Model):
+    company_name=models.CharField(max_length=500)
+    company_logo=models.ImageField(null=True, blank=True)
+    title=models.CharField(max_length=300)
+    company_website=models.URLField(max_length=500,null=True,blank=True)
+    company_location=models.CharField(max_length=200)
+    job_type=models.CharField(max_length=20,choices=JOB_TYPE)
+    created=models.DateTimeField(auto_now_add=True)
+    level=models.CharField(choices=SENIORITY,max_length=30)
+    description=models.TextField(blank=True)
+    tag=models.ManyToManyField('Tag',blank=True)
+    how_to_apply=models.CharField(max_length=200)
+    target=models.CharField(max_length=500)
+    currency=models.CharField(max_length=200,blank=True,null=True)
+    min_salary=models.PositiveIntegerField(blank=True,null=True)
+    max_salary=models.PositiveIntegerField(blank=True,null=True)
+
+class Company(models.Model):
+    company_name=models.CharField(max_length=500)
+    company_logo=models.ImageField(null=True, blank=True)
+    company_website=models.URLField(max_length=500,null=True,blank=True)
+    company_location=models.CharField(max_length=200)
+    description=models.TextField(blank=True)
+
+class Tag(models.Model):
+    name=models.CharField(max_length=100)
+
+    def save(self,*args,**kwargs):
+        self.name=f'#{self.name.lower()}'
+        return super().save(*args,**kwargs)
