7f032050c3d1cfbb10419eeead437e26b518d184
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-09 11:44:42 +0000

    added all jobs view and job detail and also added view for handling subscription

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,38 @@
+from newsletter.forms import NewsletterSubForm
+from django.db.models.query_utils import Q
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Job
+from .forms import filterJob
 
-# Create your views here.
+def jobListView(request):
+    jobs=Job.objects.all()
+    form=filterJob()
+    sub_form=NewsletterSubForm()
+    if request.POST:
+        form=filterJob(request.POST or None)
+        if form.is_valid():
+            search_term=form.cleaned_data['search']
+            print(search_term)
+            jobs=Job.objects.filter(
+                Q( title__icontains=search_term) | 
+                Q(job_type=search_term) |
+                Q(company_location__icontains=search_term)|
+                Q(description__icontains=search_term)
+                )
+
+    return render(request,'job_list.html',{
+        'jobs':jobs,
+        'form':form,
+        'sub_form':sub_form
+    })
+
+class JobDetailView(DetailView):
+    model=Job
+    slug_field='id'
+    slug_url_kwarg='id'
+    template_name='job_detail.html'
+    context_object_name='job'
+    
+
+    
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from jobs.forms import filterJob
+from jobs.models import Job
+from newsletter.forms import NewsletterSubForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def newletterSubView(request):
+    if request.POST:
+        sub_form=NewsletterSubForm(request.POST or None)
+        if sub_form.is_valid():
+            email=sub_form.cleaned_data['email']
+            print(email)
+            
+    return redirect('jobs:all_jobs')
