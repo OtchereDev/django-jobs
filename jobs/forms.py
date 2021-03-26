7ea0cb08f76db6781d8684ce7a5abda867bc3c7b
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-26 09:25:02 +0000

    feature auth: custom for sign up added

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,5 +1,14 @@
 from django import forms
+from allauth.account.forms import SignupForm
 
 class filterJob(forms.Form):
     search = forms.CharField(max_length=200)
+
+
+class CustomSignupForm(SignupForm):
+    name=forms.CharField(max_length=500)
+    def save(self,request,*args, **kwargs):
+        user=super().save(request)
+        user.name=self.cleaned_data.get('name')
+        return user
 
