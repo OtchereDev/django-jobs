8958e92968c8a19c2884769b8887dae5bab8ff06
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-27 07:23:08 +0000

    feature auth :added form for updating user details

--- django_jobs/users/forms.py
+++ django_jobs/users/forms.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
+from django.db import models
+from django.db.models import fields
 from django.utils.translation import gettext_lazy as _
-
+from django import forms
 User = get_user_model()
 
 
@@ -17,3 +19,23 @@ class UserCreationForm(admin_forms.UserCreationForm):
         error_messages = {
             "username": {"unique": _("This username has already been taken.")}
         }
+
+
+class UserUpdateForm(forms.ModelForm):
+    skills=forms.CharField( widget=forms.TextInput(attrs={'class': "form-control"}),required=False)
+    
+    languages=forms.CharField(required=False)
+    class Meta:
+        model=User
+        fields=["name",
+                "professional_title",
+                "about",
+                "profile_pic",
+                "location",
+                "job_readiness",
+                "seniority",
+                "resume",
+                "privacy",
+                "is_recruiter",]
+
+    
