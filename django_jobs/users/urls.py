7da9eeea7c2ff87ed148f7e39e26dcc8b58ed88b
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-28 07:26:00 +0000

    added url for adding experinces and education

--- django_jobs/users/urls.py
+++ django_jobs/users/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 
 from django_jobs.users.views import (
-    userDetailView, user_detail_view,
+    userDetailView, user_detail_view,UsersUpdateView,AddEductaion,AddExperience,
     user_redirect_view,
     user_update_view,
 )
 
 app_name = "users"
 urlpatterns = [
-    path('profile/',userDetailView,name='profile'),
+    path('profile/',user_detail_view,name='profile'),
+    path('profile_update/',UsersUpdateView.as_view(),name='profile_update'),
+    path('add_education/',AddEductaion.as_view(),name='add_education'),
+    path('add_experience/',AddExperience.as_view(),name='add_experience'),
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:email>/", view=user_detail_view, name="detail"),
